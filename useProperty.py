53b135fd68b9f2d81cd91b30f9ad20967c03104e
Jie <864129545zzj@gmail.com>
2018-03-07 22:41:18 +0800

    final version

--- useProperty.py
+++ useProperty.py
@@ -1,14 +1,14 @@
 class Student(object):
-    def get_score(self):
-        return self.score
 
-    def set_score(self, value):
-        if not isinstance(value, int):
-            raise ValueError('score must be an integer!')
-        if value < 0 or value > 100:
-            raise ValueError('score must between 1 ~ 100!')
-        self.score = value
+    @property
+    def score(self):
+        return self._score
+
+    #若有 property 无 setter,即为只读属性
+    @score.setter
+    def score(self, value):
+        self._score = value
 
 s = Student()
-s.set_score(60)
-print(s.get_score())
+s.score = 60
+print(s.score)
